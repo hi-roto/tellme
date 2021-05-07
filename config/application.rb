5021afe504f7557ac5eee02d13317ecc5b60a4c3
hi-roto <hirostudy2020@gmail.com>
2021-05-07 15:47:02 +0900

    アプリ名の変更

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Ideatweet
+module TellMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
