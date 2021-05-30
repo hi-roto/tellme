867d237aee4f98eef43c2d872551f0e9f8c27a0e
hi-roto <hirostudy2020@gmail.com>
2021-05-30 23:25:57 +0900

    時刻表示の修正

--- app/javascript/realtime.js
+++ app/javascript/realtime.js
@@ -1,23 +1,27 @@
-let set2digits = function digits (num) {
-  let digit;
-  if(num < 10) {digit = "0" + num;}
-  else {digit = num;}
-  return digit;
-}
+window.addEventListener('load', () => {  
+  
+    let set2digits = function digits (num) {
+    let digit;
+      if(num < 10) {digit = "0" + num;}
+      else {digit = num;}
+      return digit;
+    }
 
-let timeNow = function realtime () {
-  const nowTime = new Date();
-  const nowYear = nowTime.getFullYear();
-  const nowMonth = nowTime.getMonth() + 1;
-  const nowDay = nowTime.getDate();
-  const nowHour = set2digits(nowTime.getHours());
-  const nowMin = set2digits(nowTime.getMinutes());
-  const nowSec = set2digits(nowTime.getSeconds());
-  const dayOfWeek = nowTime.getDay();	
-  const dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek] ;	
-  const realTime = "現在:" + nowYear + "年" + nowMonth + "月" + nowDay + "日"+ "(" + dayOfWeekStr + ")"
-                 + nowHour + ":" + nowMin + ":" + nowSec;
-  document.getElementById("RealTimeArea1").innerHTML = realTime;
-  };
+  let timeNow = function realtime () {
+    const nowTime = new Date();
+    const nowYear = nowTime.getFullYear();
+    const nowMonth = nowTime.getMonth() + 1;
+    const nowDay = nowTime.getDate();
+    const nowHour = set2digits(nowTime.getHours());
+    const nowMin = set2digits(nowTime.getMinutes());
+    const nowSec = set2digits(nowTime.getSeconds());
+    const dayOfWeek = nowTime.getDay();	
+    const dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek] ;	
+    const realTime = "現在:" + nowYear + "年" + nowMonth + "月" + nowDay + "日"+ "(" + dayOfWeekStr + ")"
+                  + nowHour + ":" + nowMin + ":" + nowSec;
+    document.getElementById("RealTimeArea1").innerHTML = realTime;
+    };
 
-setInterval(timeNow, 1000); 
+  setInterval(timeNow, 1000); 
+
+});
