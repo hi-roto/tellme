fc63468e264c9b38d10179b8373eec700deaa320
hi-roto <hirostudy2020@gmail.com>
2021-05-31 23:57:21 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post '/callback', to: 'linebots#callback'
-  get "jikoku", to: 'jikokus#index'
+  get "jikokus", to: 'jikokus#index'
   root to: 'jikokus#index'
 end
